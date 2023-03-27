f6565db4f1bfdf53c9e702f0daab66468d55d118
evanrich2404 <evanrich2404@gmail.com>
2023-03-26 20:46:53 -0500

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """displays a HTML page with a list of states"""
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
