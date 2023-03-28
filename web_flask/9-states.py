720617bbcd0e124c1e16835e5b35bee857e941ea
evanrich2404 <evanrich2404@gmail.com>
2023-03-28 17:17:04 -0500

    10

---
+++ web_flask/9-states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""starts a Flask web application that displays a state"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """displays a HTML page with a list of states"""
+    states = storage.all("State")
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """displays a HTML page with a list of states"""
+    states = storage.all("State")
+    return render_template('9-states.html', states=states, id=id)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
